4c33262b448359312720280192d1495001b046bb
Jourvence <117796287+Vinqueire@users.noreply.github.com>
2024-10-03 13:00:25 +0200

    edited the search bar and added app.css for login and register

--- clothingShop/resources/views/layouts/layout.blade.php
+++ clothingShop/resources/views/layouts/layout.blade.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    @vite(['resources/css/app.css', 'resources/js/app.js']) <!-- Use the Vite directive -->
+    @vite(['resources/css/main.css', 'resources/js/app.js']) <!-- Use the Vite directive -->
 </head>
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -60,8 +60,8 @@
         <!-- Right-aligned Search Bar -->
         <div class="navbar-right ms-auto d-none d-lg-flex">
             <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+                <input id="magGlass" type="image" src="{{ asset('images/magGlass.jpg') }}" alt="Submit feedback">
+                <input id="navSearch" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             </form>
         </div>
     </div>
