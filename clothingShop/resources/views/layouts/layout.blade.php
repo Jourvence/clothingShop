2105adc74299ebdf9e0f6dd30118878b41b6d200
Jourvence <117796287+Vinqueire@users.noreply.github.com>
2024-10-08 21:32:30 +0200

    I've fixed the navbar and made it reponsive at least I hope I did

--- clothingShop/resources/views/layouts/layout.blade.php
+++ clothingShop/resources/views/layouts/layout.blade.php
@@ -11,80 +11,122 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     @vite(['resources/css/main.css', 'resources/js/app.js']) <!-- Use the Vite directive -->
+
+    <!-- Additional CSS for Centering Navbar -->
+    <style>
+        /* Ensure the center-aligned container is always centered */
+        .navbar-center {
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+        }
+
+        /* Override default Bootstrap spacing */
+        .navbar-center ul {
+            margin: 0;
+        }
+    </style>
 </head>
 <body id="colorId">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     <div id="logo" class="text-center my-3">
         <h1>
-            <!-- <a class="nav-link active" href="{{ route('main') }}">Jourvence</a>  -->
             <a href="{{ route('main') }}" class="navbar-brand">
-                <img src="{{ asset('images/clothingSiteLogo.png') }}" alt="Logo" style="width: 100px;"> 
+                <img src="{{ asset('images/clothingSiteLogo.png') }}" alt="Logo" style="width: 100px;">
             </a>
         </h1>
     </div>
-    <!-- Navbar Container -->
-    <nav class="navbar navbar-expand-lg position-relative">
-    <div class="container-fluid">
-        <!-- Centered Navigation Links Container -->
-        <div class="nav-center position-absolute top-50 start-50 translate-middle d-flex flex-row flex-wrap justify-content-center">
-            <ul class="navbar-nav d-flex flex-row justify-content-center">
-                @if (Route::has('login'))
-                    @auth
-                        <li class="nav-item mx-2">
-                            <a class="nav-link">Logged in as: <span style="color: green;"> {{ Auth::user()->name }}</span></a>
-                        </li>
-                        <li class="nav-item mx-2"> <!-- Use spacing classes like mx-2 for consistency -->
-                            <a class="nav-link active" href="{{ url('/home') }}">Home</a>          
-                        </li>
-                        <li class="nav-item mx-2">
-                            <form action="{{ url('/logout') }}" method="POST" style="display: inline;">
-                                @csrf <!-- This generates a CSRF token for the form -->
-                                <button type="submit" class="nav-link active" style="border: none; background: none; cursor: pointer;">
-                                    Logout
-                                </button>
-                            </form>
-                        </li>
-                    @else
-                        <li class="nav-item mx-2">
-                            <a class="nav-link active" href="{{ route('login') }}">Login</a>       
-                        </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item mx-2">
-                                <a class="nav-link active" href="{{ route('register') }}">Register</a>    
-                            </li>  
-                        @endif
-                    @endauth
-                @endif
-                <li class="nav-item mx-2">
-                    <a class="nav-link active" href="{{ route('tShirts') }}">T-Shirts</a>   
-                </li>  
-                <li class="nav-item mx-2">
-                    <a class="nav-link active" href="{{ route('trousers') }}">Trousers</a>    
-                </li>
-                <li class="nav-item mx-2">
-                    <a class="nav-link active" href="{{ route('shoes') }}">Shoes</a>   
-                </li>  
-            </ul>
-        </div>
 
-        <!-- Right-aligned Search Bar -->
-        <div class="navbar-right ms-auto d-none d-lg-flex">
-            <form class="d-flex" role="search">
-                <input id="magGlass" type="image" src="{{ asset('images/magGlass.jpg') }}" alt="Submit feedback">
-                <input id="navSearch" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            </form>
+    <!-- Navbar Container -->
+    <nav class="navbar navbar-expand-lg navbar-light position-relative">
+        <div class="container-fluid">
+
+            <!-- Navbar Toggler for Mobile View -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Collapsible Navbar Content -->
+            <div class="collapse navbar-collapse" id="navbarContent">
+                <!-- Left-aligned Section -->
+                <div class="navbar-nav me-auto">
+                    @if (Route::has('login'))
+                        @auth
+                            <ul class="navbar-nav d-flex flex-row">
+                                <li class="nav-item mx-2">
+                                    <a class="nav-link">Logged in as: <span style="color: blueviolet;"> {{ Auth::user()->name }}</span></a>
+                                </li>
+                                <li class="nav-item mx-2">
+                                    <a class="nav-link active" href="{{ url('/home') }}">Profile</a>
+                                </li>
+                                <li class="nav-item mx-2">
+                                    <form action="{{ url('/logout') }}" method="POST" style="display: inline;">
+                                        @csrf
+                                        <button type="submit" class="nav-link active" style="border: none; background: none; cursor: pointer;">
+                                            Logout
+                                        </button>
+                                    </form>
+                                </li>
+                            </ul>
+                        @else
+                            <ul class="navbar-nav d-flex flex-row">
+                                <li class="nav-item mx-2">
+                                    <a class="nav-link active" href="{{ route('login') }}">Login</a>
+                                </li>
+                                @if (Route::has('register'))
+                                    <li class="nav-item mx-2">
+                                        <a class="nav-link active" href="{{ route('register') }}">Register</a>
+                                    </li>
+                                @endif
+                            </ul>
+                        @endauth
+                    @endif
+                </div>
+
+                <!-- Center-aligned Navigation Links Container -->
+                <div class="navbar-center mx-auto d-none d-lg-flex">
+                    <ul class="navbar-nav d-flex flex-row justify-content-center align-items-center">
+                        <li class="nav-item mx-3">
+                            <a class="nav-link active" href="{{ route('tShirts') }}">T-Shirts</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link active" href="{{ route('trousers') }}">Trousers</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link active" href="{{ route('shoes') }}">Shoes</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- Right-aligned Search Bar -->
+                <div class="navbar-right ms-auto">
+                    <form class="d-flex" role="search">
+                        <input id="magGlass" type="image" src="{{ asset('images/magGlass.jpg') }}" alt="Submit feedback">
+                        <input id="navSearch" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    </form>
+                </div>
+            </div>
+
+            <!-- Centered Items in Mobile View -->
+            <div class="d-lg-none text-center mt-3">
+                <ul class="navbar-nav d-flex flex-row justify-content-center align-items-center">
+                    <li class="nav-item mx-3">
+                        <a class="nav-link active" href="{{ route('tShirts') }}">T-Shirts</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link active" href="{{ route('trousers') }}">Trousers</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link active" href="{{ route('shoes') }}">Shoes</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 
     <main class="py-4">
         @yield('content')
     </main>
 </body>
 </html>
-
-<!-- Add a logged in text that reads your database name and displays it like .                 You are currently logged in as : Jourvence          and the username should be green-->
- 
-    <!-- This one is probably going to be harder -->
-    <!-- make the items display from the database, the "templates" for the items, alongside It's name aka "item 1" etc get read from the database -->
--- clothingShop/resources/views/tShirts.blade.php
+++ clothingShop/resources/views/tShirts.blade.php
@@ -3,28 +3,24 @@
 @section('content')
 
 <div class="container text-center" id="mainContainerTShirts">
-  
     <div class="row" style="width: 20%;">
         <ul class="list-unstyled">
-            <h4>Categories</h4>
-            
-                <li><a class="nav-link active" href="{{ route('tShirts') }}">T-shirts</a></li>
-                <li><a class="nav-link active" href="{{ route('trousers') }}">Trousers</a></li>
-                <li><a class="nav-link active" href="{{ route('shoes') }}">Shoes</a></li>
-                <!-- Add more categories as needed -->
-            </ul>
-</div>  
-<div class="row" style="width: 90%; margin-left: 10%;" >
+            <h4 style="color:blueviolet;">Categories</h4>
+            <li><a class="nav-link active" href="{{ route('tShirts') }}">T-shirts</a></li>
+            <li><a class="nav-link active" href="{{ route('trousers') }}">Trousers</a></li>
+            <li><a class="nav-link active" href="{{ route('shoes') }}">Shoes</a></li>
+            <!-- Add more categories as needed -->
+        </ul>
+    </div>  
+    <div class="row" style="width: 90%; margin-left: 10%;" >
         @foreach ($products as $product)
             <div class="col-md-4" style="padding: 5%;">
                 <h6>{{ $product->name }}</h6>
                 <img src="{{ asset('images/' . $product->image) }}" class="img-fluid mb-3" alt="{{ $product->name }}">
                 <p>Price: {{ $product->price }}</p>
-                <a href="{{ route('products.show', $product->id) }}" class="btn btn-primary">View Details</a>
+                <a st href="{{ route('products.show', $product->id) }}" class="btn btn-dark">View Details</a>
             </div>
         @endforeach
-        </div>
-
+    </div>
 </div>
-
 @endsection
